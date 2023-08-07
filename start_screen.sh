6d41008af101fd6aa7602cbcb54f37eacadcf7a5
starlgz <97614350+starlgz@users.noreply.github.com>
2023-08-07 19:30:15 +0800

    Update start_screen.sh

--- start_screen.sh
+++ start_screen.sh
@@ -16,26 +16,33 @@ install_screen() {
 show_menu() {
     clear
     echo -e "${GREEN}=== GNU Screen 功能菜单 ===${NC}"
-    echo -e "${YELLOW}1. 创建新会话\t\t2. 查看会话列表${NC}"
-    echo -e "${YELLOW}3. 切换到会话\t\t4. 分割窗口${NC}"
-    echo -e "${YELLOW}5. 暂停/恢复会话\t6. 滚动窗口内容${NC}"
-    echo -e "${YELLOW}7. 重命名窗口\t\t8. 后台运行会话${NC}"
-    echo -e "${YELLOW}9. 共享会话\t\t10. 编辑Screen配置文件${NC}"
-    echo -e "${YELLOW}11. 显示帮助\t\t12. 显示Screen版本${NC}"
-    echo -e "${YELLOW}13. 关闭当前会话\t14. 关闭所有会话${NC}"
-    echo -e "${YELLOW}15. 切换到下一个窗口\t16. 切换到上一个窗口${NC}"
-    echo -e "${YELLOW}17. 关闭当前窗口\t18. 重置窗口计数${NC}"
-    echo -e "${YELLOW}19. 创建新窗口并切换\t20. 调整窗口大小${NC}"
-    echo -e "${YELLOW}21. 保存当前布局\t22. 加载已保存布局${NC}"
-    echo -e "${YELLOW}23. 将窗口内容导出到文件\t24. 从文件导入窗口内容${NC}"
-    echo -e "${YELLOW}25. 停止所有会话\t\t0. 退出${NC}"
+    echo -e "${YELLOW}1. 查看会话列表\t2. 创建新会话${NC}"
+    echo -e "${YELLOW}3. 暂停/恢复会话\t4. 停止所有会话${NC}"
+    echo -e "${YELLOW}0. 退出脚本${NC}"
 }
 
-# 以下是原来的功能函数，不再重复列出...
+list_sessions() {
+    screen -ls
+}
+
+create_new_session() {
+    echo -e "${YELLOW}请输入新会话的名称：${NC}"
+    read session_name
+    screen -S "$session_name"
+}
+
+toggle_pause_resume() {
+    screen -S "$STY" -X eval "pause"
+}
 
 stop_all_sessions() {
     screen -ls | grep -oP '^\d+\.[^[:space:]]+' | awk '{print $1}' | xargs -I {} screen -S {} -X quit
     echo -e "${GREEN}所有会话已停止。${NC}"
+}
+
+exit_script() {
+    echo -e "${GREEN}谢谢使用，再见！${NC}"
+    exit 0
 }
 
 main() {
@@ -45,32 +52,11 @@ main() {
         read -p "请输入选项数字： " choice
 
         case $choice in
-            1) create_new_session ;;
-            2) list_sessions ;;
-            3) switch_to_session ;;
-            4) split_window ;;
-            5) toggle_pause_resume ;;
-            6) scroll_window ;;
-            7) rename_window ;;
-            8) background_run ;;
-            9) share_session ;;
-            10) edit_config_file ;;
-            11) show_help ;;
-            12) show_version ;;
-            13) close_current_session ;;
-            14) close_all_sessions ;;
-            15) switch_to_next_window ;;
-            16) switch_to_prev_window ;;
-            17) close_current_window ;;
-            18) reset_window_count ;;
-            19) create_new_window ;;
-            20) resize_window ;;
-            21) save_layout ;;
-            22) load_layout ;;
-            23) export_window_content ;;
-            24) import_window_content ;;
-            25) stop_all_sessions ;;
-            0) echo -e "${GREEN}谢谢使用，再见！${NC}" && break ;;
+            1) list_sessions ;;
+            2) create_new_session ;;
+            3) toggle_pause_resume ;;
+            4) stop_all_sessions ;;
+            0) exit_script ;;
             *) echo -e "${GREEN}无效选项，请重新输入。${NC}" ;;
         esac
 
@@ -78,4 +64,5 @@ main() {
     done
 }
 
+echo -e "${GREEN}GNU Screen 菜单脚本 (作者: starlgz)${NC}"
 main
