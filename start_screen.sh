e94f5a95e92a9682b5ed4fd0a5b64669ec34efe8
starlgz <97614350+starlgz@users.noreply.github.com>
2023-08-07 19:36:17 +0800

    Update start_screen.sh

--- start_screen.sh
+++ start_screen.sh
@@ -20,6 +20,7 @@ show_menu() {
     echo -e "2. 创建新会话"
     echo -e "3. 暂停/恢复会话"
     echo -e "4. 停止所有会话"
+    echo -e "5. 删除所有会话"
     echo -e "0. 退出脚本"
 }
 
@@ -42,6 +43,11 @@ stop_all_sessions() {
     echo -e "${GREEN}所有会话已停止。${NC}"
 }
 
+delete_all_sessions() {
+    screen -ls | grep -oP '^\d+\.[^[:space:]]+' | awk '{print $1}' | xargs -I {} screen -S {} -X quit
+    echo -e "${GREEN}所有会话已删除。${NC}"
+}
+
 exit_script() {
     echo -e "${GREEN}谢谢使用，再见！${NC}"
     exit 0
@@ -58,6 +64,7 @@ main() {
             2) create_new_session ;;
             3) toggle_pause_resume ;;
             4) stop_all_sessions ;;
+            5) delete_all_sessions ;;
             0) exit_script ;;
             *) echo -e "${GREEN}无效选项，请重新输入。${NC}" ;;
         esac
